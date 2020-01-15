27de75997d7331ec6337cd69fbf1a2db233e25e7
Remyson Costa <remysonrodrigues@gmail.com>
2020-01-15 13:06:41 -0300

    Adicionado as funcionalidades cadastrar departamento, cadastrar professor e listar professores com mestrado.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "interacao.h"
 #include "instituicao.h"
 #include "departamento.h"
@@ -28,14 +29,28 @@
 int main(void)
 {
 
-    //VARIÁVEIS DE INTERAÇÃO
+    //VARIÁVEIS DE INTERAÇÃO MENU
     int interacao = 1;
     int op;
+
+    //VARIÁVEIS DE INTERAÇÃO PROFESSOR
+    char nome_prof[100];
+    int matricula;
+    char area_atuacao[50];
+    int titulacao;
+
+    //VARIÁVEIS DE INTERAÇÃO DEPARTAMENTO
+    char nome_depart[100];
+    char sigla[10];
+    Departamento_Arv* departamento;
+
+    //TABELA INSTITUIÇÃO
+    Hash* instituicao = instituicao_hash_cria();
 
     while(interacao)
     {
         apresenta_programa("========SISTEMA ORGANIZACIONAL INSTITUICAO DE ENSINO SUPERIOR========\n"
-                           "\n------------------SEJA BEM VINDO.Versao 1.0------------------");//'MENSAGEM' APRESENTACAO DO PROGRAMA
+                           "\n------------------SEJA BEM VINDO.Versao 1.0------------------"); //'MENSAGEM' APRESENTACAO DO PROGRAMA
         apresenta_menu(N_OPCOES, OPCAO1,TITULO_OPCAO1, TITULO_OPCAO2,TITULO_OPCAO3, TITULO_OPCAO4,
                        TITULO_OPCAO5, TITULO_OPCAO6); //APRESENTA O 'MENU'
         op = le_opcao(OPCAO1, OPCAO1 + N_OPCOES - 1); //LÊ OPCAO DIGITADA
@@ -45,17 +60,76 @@ int main(void)
 
             //OPCAO 1 CADASTRAR DEPARTAMENTO
             case OPCAO1:
-
+                        limpa_tela();
+                        printf("\nDIGITE O NOME DO DEPARTAMENTO: ");
+                        strcpy(nome_depart, le_string());
+                        printf("\nDIGITE A SIGLA DO DEPARTAMENTO: ");
+                        strcpy(sigla, le_string());
+                        if(instituicao_hash_busca_departamento(instituicao, sigla) == NULL)
+                        {
+                            departamento = departamento_cria(nome_depart, sigla);
+                            instituicao_hash_insere(instituicao, departamento);
+                            printf("\nDEPARTAMENTO CADASTRADO.\n\n");
+                        }
+                        else
+                        {
+                            printf("\nESSE DEPARTAMENTO JA SE ENCONTRA-SE CADASTRADO.\n\n");
+                        }
+                        system("pause");
+                        limpa_tela();
                         break;
 
             //OPCAO 2 CADASTRAR PROFESSOR(A)
             case OPCAO2:
+                        limpa_tela();
+                        printf("\nNOME: ");
+                        strcpy(nome_prof, le_string());
+                        printf("\nMATRICULA: ");
+                        scanf("%d", &matricula);
+                        printf("\nAREA DE ATUACAO: ");
+                        strcpy(area_atuacao, le_string());
+                        printf("\nTITULACAO (Mestrado - 1 | Doutorado - 2 | Outra - 3): ");
+                        scanf("%d", &titulacao);
+                        printf("\nDIGITE A SIGLA DO DEPARTAMENTO AO QUAL O PROFESSOR SERA INSERIDO: ");
+                        strcpy(sigla, le_string());
+                        departamento = instituicao_hash_busca_departamento(instituicao, sigla);
+                        if(departamento != NULL)
+                        {
 
+                            if(titulacao == 1)
+                            {
+                                departamento_insere_professor(departamento,
+                                                              professor_cria(nome_prof, matricula, area_atuacao, "Mestrado"));
+                            }
+                            else if(titulacao == 2)
+                            {
+                                departamento_insere_professor(departamento,
+                                                              professor_cria(nome_prof, matricula, area_atuacao, "Doutorado"));
+                            }
+                            else
+                            {
+                                departamento_insere_professor(departamento,
+                                                              professor_cria(nome_prof, matricula, area_atuacao, "Outra"));
+                            }
+
+                            printf("\nPROFESSOR CADASTRADO.\n\n");
+                            system("pause");
+                        }
+                        else
+                        {
+                            printf("\nNEM UM DEPARTAMENTO ENCONTRADO COM A SIGLA INFORMADA.\n\n");
+                            system("pause");
+                        }
+                        limpa_tela();
                         break;
 
             //OPCAO 3 LISTAR PROFESSORES COM MESTRADO
             case OPCAO3:
-
+                        limpa_tela();
+                        printf("\nPROFESSORES COM MESTRADO:\n\n");
+                        listar_departamento_professores(instituicao, 1);
+                        system("pause");
+                        limpa_tela();
                         break;
 
             //OPCAO 4 LISTAR PROFESSORES COM DOUTORADO
@@ -72,7 +146,7 @@ int main(void)
             case OPCAO6:
 
                         limpa_tela();
-                        mensagem_despedida("\n\n---OBRIGADO E ATE LOGO!---\n\n");//'MENSAGEM' DESPEDIDA DO PROGRAMA
+                        mensagem_despedida("\n\n\n\n\n\n\n\n\n<<<<---OBRIGADO E ATE LOGO!--->>>>\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                         interacao = 0;
                         break;
 
